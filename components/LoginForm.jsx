7fe9689e64c16cf43e57cbef1128061d533c19aa
Ajarek <ajarek@poczta.onet.pl>
2023-10-17 00:54:22 +0200

    added daisyui

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <form className='w-96 flex flex-col p-8 gap-4 shadow-2xl'>
+      <h1>Enter the details </h1>
+      <input type="text" placeholder="Email" className="input input-bordered input-success w-full" />
+      <input type="password" placeholder="Password" className="input input-bordered input-success w-full" />
+      <button className="btn btn-success text-white">Login</button>
+    </form>
   )
 }
 
