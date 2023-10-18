b53bb0b7dd23961709d3c7134a312b87d5108fd8
Ajarek <ajarek@poczta.onet.pl>
2023-10-18 23:32:57 +0200

    added setting

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -1,24 +1,31 @@
-"use client";
+'use client'
 
 import React from 'react'
-import { useRouter,useParams } from 'next/navigation'
+import { useRouter, useParams } from 'next/navigation'
 const UserInfo = () => {
-  
   const params = useParams()
   const router = useRouter()
-  console.log(params)
+
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">{'password'}</h2>
-    <p>email: <span>{params.user}</span></p>
-    
-    
-    <div className="card-actions justify-end">
-      <button onClick={()=>router.push('/') } className="btn btn-error">Log Out</button>
+    <div className='card w-96 bg-base-100 shadow-xl'>
+      <div className='card-body'>
+        <h2 className='card-title'>
+          {params.user.split('%')[0].toUpperCase()}
+        </h2>
+        <p>
+          email: <span>{decodeURIComponent(params.user)}</span>
+        </p>
+
+        <div className='card-actions justify-end'>
+          <button
+            onClick={() => router.push('/')}
+            className='btn btn-error'
+          >
+            Log Out
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   )
 }
 
